83e693ec49d7cdd70dfc8bcec5c84bda33fbe01c
Evan <32994398+epomp447@users.noreply.github.com>
2018-03-01 02:32:29 -0500

    Update QuickSort.java
    implemented quicksort on almost sorted array

--- QuickSort.java
+++ QuickSort.java
@@ -1,9 +1,10 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class QuickSort {
 	
 	
-	public static int n = 400;
+	public static int n = 50;//controls Size of array
 	static int numSwaps = 0;
 	static int numComps = 0;
 	/*
@@ -38,7 +39,33 @@ public class QuickSort {
 		numSwaps++;
 		return i + 1;
 	}
+	// -----------------------------------------------------------------------
+	/* Function that generates an almost sorted array */
+	public static int[] almostSort(int arr[], int n) {
+		System.out.print("\n Almost sorted:");
+		numComps=0;
+		numSwaps=0;
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i;
+		}
+		Random r = new Random();
 
+		for (int k = n / 50; k > 0; k--) {
+
+			// Pick a random index from 0 to n
+			int i = r.nextInt(n);
+			int j = r.nextInt(n);
+
+			// Swap arr[i] with the element at arr[j]
+			int temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+		}
+		// Prints the almost sorted array
+		System.out.println("\n\t"+Arrays.toString(arr));
+
+		return arr;
+	}
 	// ----------------------------------------------------------------------
 	/*
 	 * The main function that implements QuickSort() arr[] --> Array to be sorted,
@@ -75,20 +102,28 @@ public class QuickSort {
 	// ----------------------------------------------------------------------
 	// Driver program
 	public static void main(String args[]) {
+		System.out.println("******Quicksort******"+"\t n = "+n);
 		int arr[] = new int[n];
-		int n = arr.length - 1;
+		
 		Random rand = new Random();
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = rand.nextInt(100);
 		}
-		System.out.println("\n\n Quicksort: ");
+		
 
 		System.out.println("\n\n unsorted array:");
 		printArray(arr);
-		sort(arr, 0, n);
+		sort(arr, 0, n-1);
 		System.out.println("\n\n sorted array:");
 		printArray(arr);
 		System.out.println("\nSwaps: " + numSwaps);
+		System.out.println("Comparisons: " + numComps);
+		
+		int A[] = new int[n];
+		almostSort(A, n);
+		sort(A, 0, n - 1);
+		printArray(A);
+		System.out.println("Swaps: " + numSwaps);
 		System.out.println("Comparisons: " + numComps);
 	}
 }
